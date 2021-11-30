2207d759f02a921faf27152a5b0e3a55daaae3e1
JaehyunHan0722 <88475359+JaehyunHan0722@users.noreply.github.com>
2021-11-30 13:31:30 +0900

    a

--- newworkPro/src/basic/CodeTest.java
+++ newworkPro/src/basic/CodeTest.java
@@ -1,100 +1,98 @@
 package basic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Vector;
 
 public class CodeTest {
 
 	public static void main(String[] args) {
-//		Vector<Integer> number = new Vector<Integer>();
-//		Vector<Integer> test = new Vector<Integer>();
-//		for(int i=1; i<=100; i++) {
-//			number.add(i);
-//		}
-//		number.set(1, 99);
-//		System.out.println(number.get(0));
-//		System.out.println(number.get(1));
-//		System.out.println(number.size());
-//		System.out.println(number);
-//		test.add(number.get(0));
-//		test.add(number.get(1));
-//		test.add(number.get(98));
-//		test.add(number.get(99));
-//		System.out.println();
-//		System.out.println(test);
-//		test.remove(3);
-//		System.out.println(test);
 		
-//		Vector<Vector<Integer>> number = new Vector<Vector<Integer>>();
-//		Vector<Integer> temp = new Vector();
-//		for(int i=0; i<5; i++) {
-//			for(int j=51; j<=55; j++) {
-//				temp.add(j);
-//				System.out.println(temp);
-//			}
-//			temp.clear();
-//		}
-//		
-//		Vector<Integer> number = new Vector<Integer>();
-//		number.add(1);
-//		number.add(2);
-//		number.add(3);
-//		number.add(3);
-//		number.add(2);
-//		int length = number.size()/2;
-//		System.out.println(number);
-//		for(int j=1; j<=length; j++) {
-//			for(int i=0; i<number.size()-1; i++) {
-//				System.out.println(i+", "+number.get(i)+", "+number.get(i+1));
-//				if(number.get(i) == number.get(i+1)) {
-//					number.remove(i+1);
-//					number.remove(i);
-//					
-//				}
-//			}
-//		}
-//		
-//		System.out.println(number);
-
-//	    int answer = 0;
-//	    
-//	    vector<int> basket = new vector();
-//	    for(int i=0; j<moves.size(); i++){
-//	        int move = moves.get(i);
-//	        for(int j=0; j<board.size(); j++){
-//	            if(board.get(j).get(move) != 0){
-//	                basket.add(board.get(0).get(move));
-//	            }
-//	        }
-//	    }
-//	    int number = basket.size()/2;
-//	    for(int i=1; i<=number; i++){
-//	        for(int j=0; j<basket.size()-1; j++){
-//	            if(basket.get(j)==basket.get(j+1)){
-//	                basket.remove(j+1);
-//	                basket.remove(j);
-//	                answer = answer + 2;
-//	            }
-//	        }
-//	    }
-//	    return answer;
+		int[][]board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+		int[] moves = {1,5,3,5,1,2,2,4};
 		
-		int[] number = {1,2,3};
+		int answer = 0;
 		
+		Vector<Integer> basket = new Vector();
+		for(int i=0; i<moves.length; i++){
+            int move = moves[i]-1;
+            for(int j=0; j<board.length; j++){
+                if(board[j][move] != 0){
+                    basket.add(board[j][move]);
+                    board[j][move] = 0;
+                    
+                    break;
+                }
+                
+            }
+            
+        }
 		
-		
-		for(int i=0; i<number.length; i++) {
-			
-			for(int j=0; j<number.length; j++) {
-				if(i ==1 ) {
-					i = 3;
-				}
-				System.out.println(i+" "+j);
-				
-			}
+		boolean delete = false;
+		int count = 0;
+		for(int i=0; i<basket.size(); i++) {
+			count +=  i;
 		}
+		System.out.println(count);
+		System.out.println("바스켓: "+basket);
+		int i = 0;
+		while(i<count) {			
+			System.out.println("테스트전: "+basket);
+			if(basket.size() == 0) {
+				break;
+			}
+			if(delete) {
+				i = 0;
+			}
+			System.out.println("i값 "+i);
+			System.out.println("바스켓 길이: "+basket.size());
+			if(basket.size() == 0) {
+				break;
+			}
+            int next = i+1;
+            System.out.println("테스트 하는 숫자: "+basket.get(i)+", "+basket.get(next));
+        	if(basket.get(i) == basket.get(next)) {
+        		basket.remove(next);
+        		basket.remove(i);
+        		answer += 2;
+        		delete = true;
+        		System.out.println("지웠다!!");
+        	}else {
+        		System.out.println("안지웠다!!");
+        		delete = false;
+        	}
+        	i++;
+        	System.out.println("테스트후: "+basket);
+        	System.out.println("==============================================");	      
+		}
+//		for(int i=0; i<count; i++){
+//			System.out.println("테스트전: "+basket);
+//			if(delete) {
+//				i = start;
+//			}
+//			System.out.println("i값 "+i);
+//			System.out.println("바스켓 길이: "+basket.size());
+//			if(basket.size() == 0) {
+//				break;
+//			}
+//            int next = i+1;
+//            System.out.println("테스트 하는 숫자: "+basket.get(i)+", "+basket.get(next));
+//        	if(basket.get(i) == basket.get(next)) {
+//        		basket.remove(next);
+//        		basket.remove(i);
+//        		answer += 2;
+//        		delete = true;
+//        		System.out.println("지웠다!!");
+//        	}else {
+//        		System.out.println("안지웠다!!");
+//        		delete = false;
+//        	}
+//        	System.out.println("테스트후: "+basket);
+//        	System.out.println("==============================================");
+//        }
 		
-		
+		System.out.println(answer);
+	
 	}
 	    
 }
